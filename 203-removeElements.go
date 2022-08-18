ded287df5f8d42a16a435bc7952c3b3e71e108c3
soul_of_world <zkyaoyuanyuan@gmail.com>
2022-08-18 16:07:26 +0800

    链表

---
+++ 203-removeElements.go
@@ -0,0 +1,20 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func removeElement(head *ListNode, val int) *ListNode {
+	points := &ListNode{}
+	points.Next = head
+	cur := points
+	for cur != nil && cur.Next != nil {
+		if cur.Next.Val == val {
+			cur.Next = cur.Next.Next
+		} else {
+			cur = cur.Next
+		}
+	}
+	return points.Next
+}
