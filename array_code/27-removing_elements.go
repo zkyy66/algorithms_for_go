a708929e26dc7d4939affdb70ed6c636906a78e0
疯猫(CrazyCat) <zkyaoyuanyuan@gmail.com>
2025-03-05 10:58:58 +0800

    add 27 and 151

--- array_code/27-removing_elements.go
+++ array_code/27-removing_elements.go
@@ -8,7 +8,6 @@ package array_code
 // 快指针：寻找新数组的元素 ，新数组就是不含有目标元素的数组
 // 慢指针：指向更新 新数组下标的位置
 // 很多同学这道题目做的很懵，就是不理解 快慢指针究竟都是什么含义，所以一定要明确含义，后面的思路就更容易理解了。
-// 27
 func removeElement(nums []int, val int) int {
 	numsLen := len(nums)
 	if numsLen <= 0 {
---
+++ string_code/151-reverseWords.go
@@ -0,0 +1,54 @@
+package string_code
+
+import "strings"
+
+// versionOne
+func reverseWordsOne(s string) string {
+	t := strings.Fields(s)
+	for i := 0; i < len(t)/2; i++ {
+		t[i], t[len(t)-1-i] = t[len(t)-1-i], t[i]
+	}
+	newStr := strings.Join(t, " ")
+	return newStr
+}
+func reverseWordsTwo(s string) string {
+	slowIndex, fastIndex := 0, 0
+	b := []byte(s)
+	for len(b) > 0 && fastIndex < len(b) && b[fastIndex] == ' ' {
+		fastIndex++
+	}
+	for ; fastIndex < len(b); fastIndex++ {
+		if fastIndex-1 > 0 && b[fastIndex-1] == b[fastIndex] && b[fastIndex] == ' ' {
+			continue
+		}
+		b[slowIndex] = b[fastIndex]
+		slowIndex++
+	}
+	if slowIndex-1 > 0 && b[slowIndex-1] == ' ' {
+		b = b[:slowIndex-1]
+	} else {
+		b = b[:slowIndex]
+	}
+	reverse(&b, 0, len(b)-1)
+	i := 0
+	for i < len(b) {
+		j := i
+		for ; j < len(b) && b[j] != ' '; j++ {
+			reverse(&b, i, j-1)
+		}
+		i = j
+		i++
+	}
+	return string(b)
+}
+func reverse(b *[]byte, left, right int) {
+	for left < right {
+		(*b)[left], (*b)[right] = (*b)[right], (*b)[left]
+		left++
+		right--
+	}
+}
+func main() {
+	reverseWordsOne("yaoyuan")
+	reverseWordsTwo("yaoyuan")
+}
