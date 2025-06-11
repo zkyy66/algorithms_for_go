763b5e96d253b088effeaafe381680eed2b808dd
疯猫(CrazyCat) <zkyaoyuanyuan@gmail.com>
2025-06-11 21:31:15 +0800

    feat:add

--- binary_tree/144-二叉树前序遍历.go
+++ binary_tree/144-二叉树前序遍历.go
@@ -18,6 +18,58 @@ func preorderTraversal(root *TreeNode) []int {
 	return stack
 }
 
-func preorderTraversalForStack(root *TreeNode) []int {
-	return nil
+//迭代方式
+//前序
+func preorderTraversalForStack(root *TreeNode) (res []int) {
+	stack := []*TreeNode{}
+	node := root
+	for node != nil || len(stack) > 0 {
+		for node != nil {
+			res = append(res, node.Val)
+			stack = append(stack, node)
+			node = node.Left
+		}
+		node = stack[len(stack)-1].Right
+		stack = stack[:len(stack)-1]
+	}
+	return
+}
+
+//中序
+func inorderTraversalForStack(root *TreeNode) (res []int) {
+	stack := []*TreeNode{}
+	for root != nil || len(stack) > 0 {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		root = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		res = append(res, root.Val)
+		root = root.Right
+	}
+	return
+}
+
+//后序
+func postorderTraversalForStack(root *TreeNode) (res []int) {
+	stk := []*TreeNode{}
+	var prev *TreeNode
+	for root != nil || len(stk) > 0 {
+		for root != nil {
+			stk = append(stk, root)
+			root = root.Left
+		}
+		root = stk[len(stk)-1]
+		stk = stk[:len(stk)-1]
+		if root.Right == nil || root.Right == prev {
+			res = append(res, root.Val)
+			prev = root
+			root = nil
+		} else {
+			stk = append(stk, root)
+			root = root.Right
+		}
+	}
+	return
 }
